1c0d4623db4a8b80571b81a6277748e614a1f657
Adrian <addycraft@gmx.de>
2025-06-05 15:34:02 +0200

    Login, Registration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,24 @@
     let { children } = $props();
     import "../app.css";
     import Header from "../lib/components/ui/Header.svelte";
+    import {onMount} from "svelte";
 
     let profileData = 1;
+    let isSearchBarHidden = $state(false);
+
+    const hideSearchBarAt = [
+        "/login",
+        "/register"
+    ];
+
+    onMount(() => {
+        const url = new URL(document.URL);
+        if(hideSearchBarAt.includes(url.pathname)) {
+            isSearchBarHidden = true;
+        }
+    });
 </script>
 
-<Header profileData={profileData} />
-<div class="h-screen">
-    {@render children()}
-</div>
+<Header profileData={profileData} hideSearchBar={isSearchBarHidden} />
+ {@render children()}
 <Footer profileData={profileData} />
