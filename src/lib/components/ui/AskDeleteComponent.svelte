e2dfb77eb7af822d0cd9191d8ae08f78d9b58ab9
adrku24 <a@b.c>
2025-06-13 00:05:06 +0200

    Favorites site

---
+++ src/lib/components/ui/AskDeleteComponent.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let callbackYes;
+    export let callbackNo;
+    export let question;
+    export let questionClasses;
+
+    export let yesButtonText;
+    export let yesButtonClasses;
+    export let noButtonText;
+    export let noButtonClasses;
+</script>
+
+<div class="toast toast-center mb-15">
+    <div class="rounded-box shadow-sm mx-4">
+        <p class="w-full px-2 pb-1 mt-1 {questionClasses} border-b">{question}</p>
+        <p class="{yesButtonClasses}" style="cursor: pointer !important;" onclick={() => callbackYes()}>{yesButtonText}</p>
+    </div>
+
+    <div class="rounded-box shadow-sm mx-4">
+        <p class="{noButtonClasses}" style="cursor: pointer !important;" onclick={() => callbackNo()}>{noButtonText}</p>
+    </div>
+</div>
---
+++ src/lib/components/ui/GoBackItem.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { X } from "@lucide/svelte";
+
+    export let href;
+</script>
+
+<div class="flex justify-end">
+    <a class="shadow-sm rounded-box m-2" href={href}>
+        <X size={24} />
+    </a>
+</div>
