da37d08424159b0e1a1aadc73fe0b3a6f40fe757
adrku24 <a@b.c>
2025-06-12 21:54:23 +0200

    Footer changes

--- src/lib/components/ui/Footer.svelte
+++ src/lib/components/ui/Footer.svelte
@@ -14,8 +14,11 @@
 <div class="mt-15"></div>
 <div class="dock dock-sm">
     <a href="/">
-        <House size="1.2em"/>
-        <span class="dock-label">Start</span>
+        <svg width="19.2" height="19.2" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/>
+            <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+        </svg>
+        <span class="dock-label font-bold">Start</span>
     </a>
 
     {#if profileData}
@@ -37,12 +40,12 @@
 
         <a href="/chat">
             <MessageCircleMore size="1.2em"/>
-            <span class="dock-label">Chat</span>
+            <span class="dock-label font-bold">Chat</span>
         </a>
 
         <a href="/profile">
             <CircleUser size="1.2em"/>
-            <span class="dock-label">Profil</span>
+            <span class="dock-label font-bold">Profil</span>
         </a>
     {:else}
         <a href="/login">
