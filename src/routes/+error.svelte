f568243a69e62519a509345241a653b19815fb0b
adrku24 <a@b.c>
2025-06-13 02:38:46 +0200

    Error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<div class="flex h-screen">
+    <div class="m-auto">
+        <p class="text-3xl text-error text-center">404</p>
+        <p class="text-lg text-center mx-16">Seite nicht gefunden.</p>
+        <div class="flex justify-center">
+            <a class="btn btn-accent text-xs" href="/">Zurück zur Homepage</a>
+        </div>
+    </div>
+</div>
