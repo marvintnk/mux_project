5e989974ac8fac510f3ce839ffeb5a7b14cac470
adrku24 <a@b.c>
2025-06-14 23:38:12 +0200

    Finished chat template

--- src/lib/components/ui/Rating.svelte
+++ src/lib/components/ui/Rating.svelte
@@ -7,8 +7,8 @@
 
 <div class="rating {ratingSizeClass} rating-half">
     <input type="radio" name="rating-11" class="rating-hidden hidden" />
-    {#each {length: parseInt(MAX_RATING)} as _, i}
-        {#if (parseInt(rating * 2)) === (i + 1)}
+    {#each {length: MAX_RATING} as _, i}
+        {#if (rating * 2) === (i + 1)}
             <input type="radio" name="rating-11" class="mask mask-star-2 mask-half-{(i % 2) + 1} bg-amber-400" aria-label="{i / 2} star" checked="checked" disabled="{!editable}" />
         {:else}
             <input type="radio" name="rating-11" class="mask mask-star-2 mask-half-{(i % 2) + 1} bg-amber-400" aria-label="{i / 2} star" disabled="{!editable}"/>
