ee3a054b9203b1f920e44d17cecbb0bce0bda5af
adrku24 <a@b.c>
2025-07-17 18:01:55 +0200

    Footer responsive

--- src/lib/components/ui/Footer.svelte
+++ src/lib/components/ui/Footer.svelte
@@ -7,7 +7,7 @@
         CircleUser,
         ContactRound
     } from "@lucide/svelte";
-    
+
     // Vereinfachte Navigation
     async function navigateWithAuth(path) {
         await invalidateAll();
@@ -16,37 +16,45 @@
 </script>
 
 <div class="mt-15"></div>
-<div class="dock dock-sm">
-        <!-- Nur für eingeloggte Benutzer -->
-        <button onclick={() => navigateWithAuth('/')}>
-            <svg width="19.2" height="19.2" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/>
-                <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
-            </svg>
-            <span class="dock-label font-bold">Start</span>
-        </button>
+<div class="dock dock-xs">
+    <!-- Nur für eingeloggte Benutzer -->
+    <button onclick={() => navigateWithAuth('/')}>
+        <svg width="19.2" height="19.2" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/>
+            <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+        </svg>
+        <span class="dock-label font-bold">Start</span>
+    </button>
 
-        <button onclick={() => navigateWithAuth('/favorites')}>
-            <BookHeart size="1.2em"/>
-            <span class="dock-label">Favoriten</span>
-        </button>
+    <button onclick={() => navigateWithAuth('/favorites')}>
+        <BookHeart size="1.2em"/>
+        <span class="dock-label">Favoriten</span>
+    </button>
 
-        <button onclick={() => navigateWithAuth('/add')}>
-            <CirclePlus size="1.2em"/>
-            <span class="dock-label">Hinzufügen</span>
-        </button>
+    <button onclick={() => navigateWithAuth('/add')}>
+        <CirclePlus size="1.2em"/>
+        <span class="dock-label">Hinzufügen</span>
+    </button>
 
-        <button onclick={() => navigateWithAuth('/chat')}>
-            
-            <span class="indicator">
-                <MessageCircleMore size="1.2em"/>
-                <!-- <span class="badge badge-accent badge-xs shadow-sm indicator-item"></span> -->
-            </span>
-            <span class="dock-label font-bold">Chat</span>
-        </button>
+    <button onclick={() => navigateWithAuth('/chat')}>
 
-        <button onclick={() => navigateWithAuth('/profile')}>
-            <CircleUser size="1.2em"/>
-            <span class="dock-label font-bold">Profil</span>
-        </button>
+        <span class="indicator">
+            <MessageCircleMore size="1.2em"/>
+            <!-- <span class="badge badge-accent badge-xs shadow-sm indicator-item"></span> -->
+        </span>
+        <span class="dock-label font-bold">Chat</span>
+    </button>
+
+    <button onclick={() => navigateWithAuth('/profile')}>
+        <CircleUser size="1.2em"/>
+        <span class="dock-label font-bold">Profil</span>
+    </button>
 </div>
+
+<style>
+    @media (max-width: 256px) {
+        .dock-label {
+            display: none !important;
+        }
+    }
+</style>
